24e3e6a8c0bb420612c32c67085eb65e115dcb1e
Your Name <you@example.com>
2020-07-26 15:54:51 +0800

    'add'

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">自定义事件（下拉菜单）</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> | 
+      <router-link to="/pageOne">瀑布流布局</router-link>
     </div>
     <router-view/>
   </div>
@@ -19,6 +20,7 @@
 
 #nav {
   padding: 30px;
+  border: solid 1px #000;
   a {
     font-weight: bold;
     color: #2c3e50;
---
+++ src/views/pageOne.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="box">
+    <h3>css瀑布流布局1</h3>
+    <div class="cainter">
+        <div class="item" v-for="(item,index) in list"  :key="index">
+            <img src="https://picsum.photos/130/200" alt="">
+        </div>
+    </div>
+    <h3>css瀑布流布局2</h3>
+
+</div>
+    
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            list:[1,2,3,4,5,6,7,8,9,78]
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.cainter{
+    column-count: 2;
+    column-gap:5px;
+}
+.item{
+    position: relative;
+    margin-bottom: 5px;
+    counter-increment: item-counter;//数字递增的类名样式
+}
+.item::after{
+    position: absolute;
+    top: 5px;
+    left: 5px;
+    width: 20px;
+    height: 20px;
+    background: #000;
+    line-height: 20px;
+    text-align: center;
+    color:#fff;
+    content: counter(item-counter);//定义一个类名
+
+}
+.item img{
+    display: block;
+    width: 100%;
+    height: 100%;
+}
+.item:first-child{
+    height: 300px;
+}
+.box{
+    margin: 10px;
+    border: 1px solid #000;
+}
+</style>
